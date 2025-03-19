f166c6d2fedb062ff7786fada76ab2720caaecb1
YannC <ycoornaert@kestra.io>
2025-03-19 09:00:35 +0100

    chore: add package-info file

---
+++ src/main/java/io/kestra/plugin/templates/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This subgroup of plugins contains example tasks for creating Kestra tasks and triggers.",
+    categories = { PluginSubGroup.PluginCategory.TOOL}
+)
+package io.kestra.plugin.templates;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
